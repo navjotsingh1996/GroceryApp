152a0bce6f5af4dac01f15050512653633a0d42d
navjotsingh1996 <navjotsingh1996@aol.com>
2018-05-31 14:49:21 -0700

    adding both forms and support for adding more groceries

--- src/pages/listView/listView.js
+++ src/pages/listView/listView.js
@@ -6,11 +6,12 @@ import Subheader from 'material-ui/Subheader';
 import Checkbox from 'material-ui/Checkbox';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
 import cookie from 'react-cookies'
 import "react-table/react-table.css";
-
+/*
 export const fakeData = [
     {date: "10/10/10", name: "test", groceries: [{name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "ListViewles", date: "1/1/1", amount: 5, checked: false}, {name: "blueberries", date: "1/1/1", amount: 5, checked: false}, {name: "strawberries", date: "1/1/1", amount: 5, checked: false}, {name: "berries", date: "1/1/1", amount: 5, checked: false}]},
     {date: "1/2/18", name: "test1", groceries: [{name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}]},
@@ -21,21 +22,24 @@ export const fakeData = [
     {date: "03/04/59", name: "zeta", groceries: [{name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false} ]},
     {date: "11/11/25", name: "1dfs", groceries: [{name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}, {name: "carrots", date: "1/1/1", amount: 5, checked: false}]},
 ];
-
+*/
 class ListView extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             data: [],
-            selected: fakeData[0],
+            selected: {},
             delDialog: false,
             formDialog: false,
             grocDialog: false,
             formData: {
                 name: "",
+                nameErr: "",
                 date: "",
+                dateErr: "",
                 itemNums: "",
+                itemErr: "",
                 items: [],
             },
             expanded: []
@@ -46,21 +50,28 @@ class ListView extends Component {
       this.state.data.map((d,i) => {
         cookie.save('grocery_app_data'+i, d, {path:"/"});
       });
+        cookie.save('grocery_app_rows', this.state.data.length, {path: "/"});
+    };
+
+    readjustDataToCookies = (deleteIndex) => {
+        cookie.remove('grocery_app_data'+deleteIndex, {path: "/"});
+        this.state.data.map((d,i) => {
+            cookie.save('grocery_app_data'+i, d, {path:"/"});
+        });
+        cookie.save('grocery_app_rows', this.state.data.length, {path: "/"});
     };
 
     componentWillMount () {
-        this.setState({data: fakeData});
+      //  this.setState({data: fakeData});
         const rows = parseInt(cookie.load('grocery_app_rows'));
         const data = [];
         for (var i = 0; i < rows; i++) {
+            if (i === 0) this.setState({selected: cookie.load('grocery_app_data0')});
+            console.log(cookie.load('grocery_app_data'+i));
             data.push(cookie.load('grocery_app_data'+i));
         }
-
-        if (data && data.length !== 0) {
-            this.setState({data});
-        } else {
-            this.setState({data: fakeData});
-        }
+        console.log(data);
+        this.setState({data});
     }
 
     checkOffItems =(groceryIndex, dataIndex) => {
@@ -73,7 +84,7 @@ class ListView extends Component {
         return (
             list.map((grocery, index) => {
                 const realDataIndex = firstArr ? index : Math.ceil(this.state.data[listIndex].groceries.length/2) + index;
-                return (<ListItem key={index} primaryText={`${grocery.amount} ${grocery.name}`} leftCheckbox={
+                return (<ListItem key={index} primaryText={`${grocery.date} ${grocery.amount} ${grocery.name}`} leftCheckbox={
                     <Checkbox
                         onCheck={() => {this.checkOffItems(realDataIndex, listIndex)}}
                         checked={this.state.data[listIndex].groceries[realDataIndex].checked}
@@ -81,6 +92,14 @@ class ListView extends Component {
                 } />)
             })
         );
+    };
+
+    getCurrentDate = () => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const date = today.getDate().toString().length == 2 ? today.getDate() : '0'+today.getDate();
+        const month = today.getMonth().toString().length == 2 ? today.getMonth() : '0'+today.getMonth();
+        return month+'/'+date+'/'+year;
     };
 
     delCell (row) {
@@ -130,6 +149,7 @@ class ListView extends Component {
         });
         const newData = this.state.data;
         newData.splice(index,1);
+        this.readjustDataToCookies(index);
         this.setState({data: newData, delDialog: false});
     };
 
@@ -155,8 +175,24 @@ class ListView extends Component {
 
     formDataItemNums = (evt, val) => {
         const formData = this.state.formData;
-        formData.itemNums = val;
-        this.setState({formData: formData});
+        if (isNaN(val)) {
+            formData.itemErr = "You can only insert numbers in this field";
+        } else {
+            formData.itemNums = parseInt(val);
+            formData.itemErr = "";
+            const items = [];
+            for (var i = 0; i < parseInt(val); i++) {
+                items.push({name: "", quantity: "", exp: ""});
+            }
+            formData.items = items;
+        }
+        this.setState({formData});
+    };
+
+    addItem = () => {
+        const formData = this.state.formData;
+        formData.items.push({name: "", quantity: "", exp: ""});
+        this.setState({formData});
     };
 
     render() {
@@ -166,7 +202,9 @@ class ListView extends Component {
             <RaisedButton label="NO" secondary onClick={this.closeDelDialog} />,
         ];
         const createListActions = [
-            <RaisedButton label="NEXT" primary onClick={this.openGrocDialog} />,
+            <RaisedButton label="NEXT" primary onClick={this.openGrocDialog} disabled={
+                !!(this.state.formData.dateErr || this.state.formData.nameErr || this.state.formData.itemErr && !this.state.formData.date || !this.state.formData.name)
+            } />,
             <RaisedButton label="CANCEL" secondary onClick={this.closeFormDialog} />,
         ];
         const grocActions = [
@@ -193,9 +231,9 @@ class ListView extends Component {
                     open={this.state.formDialog}
                     onRequestClose={this.closeFormDialog}
                 >
-                    <TextField floatingLabelText="Date" hintText="EX: 10/10/10" value={this.state.formData.date} onChange={this.formDataDate}/><br /><br />
-                    <TextField floatingLabelText="Name" value={this.state.formData.name} onChange={this.formDataName}/><br /><br />
-                    <TextField floatingLabelText="Number of grocery items" value={this.state.formData.itemNums} onChange={this.formDataItemNums}/>
+                    <TextField floatingLabelText="Date" hintText="EX: 10/10/10" errorText={this.state.formData.dateErr} value={this.getCurrentDate()} onChange={this.formDataDate}/><br /><br />
+                    <TextField floatingLabelText="Name" value={this.state.formData.name} errorText={this.state.formData.nameErr} onChange={this.formDataName}/><br /><br />
+                    <TextField floatingLabelText="Number of grocery items" value={this.state.formData.itemNums} errorText={this.state.formData.itemErr} onChange={this.formDataItemNums}/>
                 </Dialog>
                 <Dialog
                     title="List of Groceries"
@@ -203,8 +241,19 @@ class ListView extends Component {
                     modal={false}
                     open={this.state.grocDialog}
                     onRequestClose={this.closeGrocDialog}
+                    autoScrollBodyContent
                 >
-                    This will have the grocery list
+                    {this.state.formData.items.map((d,i) => {
+                        return (<div key={i}>
+                                    <h2>{i+1}</h2><br />
+                                    <TextField floatingLabelText="Name" /><br />
+                                    <TextField floatingLabelText="Quantity" /><br />
+                                    <TextField floatingLabelText="Expiration Date" /><br />
+                                </div>)
+                    })}
+                    <FloatingActionButton >
+                        <ContentAdd onClick={this.addItem}/>
+                    </FloatingActionButton>
                 </Dialog>
                 <br /><br /><br />
                 <ReactTable
